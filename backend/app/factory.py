cf8fb8e37f541aa68d93b6b3c1362d7b2e8cd51f
Brandy Dan <brandydan2002@gmail.com>
2026-02-13 17:24:19 +0700

    Implement factory method for backend

---
+++ backend/app/factory.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from backend.api import pypi
+from backend.models import Package
+from backend.database import create_db_and_tables
+
+
+def create_app() -> FastAPI:
+
+    app = FastAPI(title="Local Package Registry")
+
+    @app.on_event("startup")
+    def on_startup():
+        create_db_and_tables()
+
+    @app.get("/")
+    def root():
+        return {
+            "status": "ok",
+        }
+
+    app.include_router(pypi.router, prefix="/pypi")
+    # app.include_router(npm.router, prefix="/npm")
+
+    return app 
+
